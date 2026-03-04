4490acff506795d7eec39140c659a6fbf86c5708
RWeinswig <ryan.weinswig@menloschool.org>
2026-03-04 09:51:15 -0800

    Revert "Added feature firstName. Can you review and accept this request?"

--- src/Backend.java
+++ src/Backend.java
@@ -1,5 +1,3 @@
-import java.util.Scanner;
-
 public class Backend {
     private Frontend window;
 
@@ -10,11 +8,5 @@ public class Backend {
     public static void main(String[] args) {
         Backend backend = new Backend();
     }
-    public String getUserName(){
-        Scanner scan = new Scanner(System.in);
-        System.out.print("Enter your first name: ");
-        return scan.nextLine();
-    }
-
 
 }
