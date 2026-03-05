106e7257f7f6f3c06da05e02849f528275ca85a9
meramurphy <mera.murphy@menloschool.org>
2026-03-05 14:00:57 -0800

    favorite meal

--- src/Backend.java
+++ src/Backend.java
@@ -1,4 +1,7 @@
+import java.util.Scanner;
+
 public class Backend {
+    Scanner s = new Scanner(System.in);
     private Frontend window;
 
     public Backend() {
@@ -7,6 +10,12 @@ public class Backend {
 
     public static void main(String[] args) {
         Backend backend = new Backend();
+    }
+
+    public String favMeal(){
+        System.out.print("What is your favorite meal?");
+        String favmeal = s.nextLine();
+        return favmeal;
     }
 
 }
--- src/Frontend.java
+++ src/Frontend.java
@@ -31,5 +31,8 @@ public class Frontend extends JFrame {
         g.drawString("About You", (WINDOW_WIDTH/2)-(HEADER_FONT_SIZE*2), 200);
 
         // TODO: Paint Personal Information Here
+        String printer = backend.favMeal();
+        g.drawString(printer, 500, 500);
+
     }
 }
